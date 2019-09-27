481ad11f72b84fc70d18656f924f8b02b5ff911d
alirezaarv <alireza.arv.mail@gmail.com>
2019-09-27 17:10:01 +0330

    words_freq_one

--- language_skills/utilities.py
+++ language_skills/utilities.py
@@ -1,30 +1,21 @@
+def freq(string_arr):
+    result = []
+    for word in set(string_arr):
+        result.append((word, string_arr.count(word)))
+    return result
+
+def freq_count(string, freq_number):
+    result = 0
+    for (word, count) in freq(string):
+        if count == freq_number:
+            result += 1
+    return result
 
 
-
-def filterTheDict(dictObj, callback):
-    newDict = dict()
-    # Iterate over all the items in dictionary
-    for (key, value) in dictObj.items():
-        # Check if item satisfies the given condition then add to new dict
-        if callback((key, value)):
-            newDict[key] = value
-    return newDict
-
+# شمارش تعداد واژههای با بسامد ١ در هر متن
 def words_frequency_one(words):
     great_string = []
     for word in words:
         great_string.append(word['wordForm'])
-    str2 = [] 
-    for i in great_string:              
-        # checking for the duplicacy 
-        if i not in str2: 
-            # insert value in str2 
-            str2.append(i)
-            
-    result = 0
-    for i in range(0, len(str2)): 
-        if great_string.count(str2[i]) == 1:
-            result += 1
+    result = freq_count(great_string, 1)
     return result
-
-        
