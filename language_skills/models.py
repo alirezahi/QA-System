3a258ea85a548124a85e8a76ad561e0188ac8139
Alireza Heydari <alireza97hi@gmail.com>
2019-10-03 15:50:14 +0330

    add dashboard

--- language_skills/models.py
+++ language_skills/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class QAUser(User):
+    pass
+
+
+class AbstractAnswer(models.Model):
+    text = models.CharField(max_length=200)
+    order = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class Text(models.Model):
+    text = models.TextField()
+    level = models.CharField(max_length=4)
+
+
+class OptionAnswer(models.Model):
+    text = models.TextField()
+
+
+class MCAnswer(AbstractAnswer):
+    start = models.IntegerField()
+    end = models.IntegerField()
+    options = models.ManyToManyField(OptionAnswer)
+    
+
+class MultipleChoiceQuestion(models.Model):
+    text = models.TextField()
+    level = models.CharField(max_length=4)
+    answers = models.ForeignKey(MAAnswer, on_delete=models.CASCADE)
+    origin_text = models.ForeignKey(Text, on_delete=models.CASCADE)
+
+
+class VacancyAnswer(AbstractAnswer):
+    start = models.IntegerField()
+    end = models.IntegerField()
+    text = models.TextField()
+
+
+class VacancyQuestion(models.Model):
+    text = models.TextField()
+    level = models.CharField(max_length=4)
+    answers = models.ForeignKey(VacancyAnswer, on_delete=models.CASCADE)
+    origin_text = models.ForeignKey(Text, on_delete=models.CASCADE)
--- language_skills/utilities.py
+++ language_skills/utilities.py
@@ -1,15 +1,6 @@
 
 
 
-def filterTheDict(dictObj, callback):
-    newDict = dict()
-    # Iterate over all the items in dictionary
-    for (key, value) in dictObj.items():
-        # Check if item satisfies the given condition then add to new dict
-        if callback((key, value)):
-            newDict[key] = value
-    return newDict
-
 def words_frequency_one(words):
     great_string = []
     for word in words:
@@ -20,7 +11,7 @@ def words_frequency_one(words):
         if i not in str2: 
             # insert value in str2 
             str2.append(i)
-            
+
     result = 0
     for i in range(0, len(str2)): 
         if great_string.count(str2[i]) == 1:
