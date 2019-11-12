49b54bb0b1ad851d601c21ffba8cd9697d55010a
Alireza Heydari <alireza97hi@gmail.com>
2019-11-12 22:32:22 +0330

    add trace

--- language_skills/utilities.py
+++ language_skills/utilities.py
@@ -263,7 +263,7 @@ class Analyser():
         for index, row in self.words.iterrows():
             if not math.isnan(float(row['q'])):
                 try:
-                    if not math.isnan(row['SyllableNumber']) and not isinstance(row['SyllableNumber'], str):
+                    if not math.isnan(int(row['SyllableNumber'])) and not isinstance(row['SyllableNumber'], str):
                         result += row['SyllableNumber']
                 except:
                     import pdb;pdb.set_trace()
